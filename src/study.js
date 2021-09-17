b9596d565e3f37f7ec613825e7dac2e8557ba35e
itoNano <itokey120+1@gmail.com>
2021-09-18 07:47:05 +0900

    ファイル名修正

--- src/study.js
+++ src/study.js
@@ -1,59 +1,61 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './assets/scss/index.scss';
-import OnlineGame from './online_game'
-import OfflineGame from './offline_game'
-import { Header } from "./components";
-// import { firebaseApp, firebaseStore, firebaseAuth } from './firebase.js';
-// import firebase from 'firebase/app';
-// 認証周りやDB周りで必要なためimportしておく
-import 'firebase/auth';
-import 'firebase/database';
-
-const Body = (props) => {
-    const [error, setError] = useState('');
-    const now = props.set;
-    const callError = () => {
-        setError('何らかのエラーが発生しました。');
-        props.methodsForState(null);
-    };
-    const selectGame = (set) => {
-        setError('');
-        props.methodsForState(set);
-    };
-    if (now === null) {
-        return (
-            <div className="gameRoomWrapper">
-                <div>{error}</div>
-                <div className="gameRoomWrapper__button" onClick={() => selectGame('offline')}>
-                    オフラインで遊ぶ
-                </div>
-                <div className="gameRoomWrapper__button" onClick={() => selectGame('online')}>
-                    オンラインで遊ぶ
-                </div>
-            </div>
-        );
-    } else if (now === 'offline') {
-        return <OfflineGame />;
-    } else {
-        return <OnlineGame callError={callError} />;
+import ChapterFour from './chapter4/ChapterFour'
+import ChapterFive from './chapter5/ChapterFive'
+import ChapterSix from './chapter6/ChapterSix'
+class TodoApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
-};
 
+    render() {
+        return (
+            <React.Fragment>
+                <div>react.fragmentを使用することでrenderした時に出力されないタグを使用できる</div>
+                <Fragment />
+                <ClassComponents />
+                <FunctionComponents />
+                <ChapterFour title="propsの受け渡し方" />
+                <ChapterFive />
+                <ChapterSix />
+            </React.Fragment>
+        );
+    }
+}
+class Fragment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-const Room = (props) => {
-    const [forState, setForState] = useState(null);
-    const methodsForState = (set) => {
-        setForState(set);
-    };
+    render() {
+        return (
+            <>
+                <div>もしくはこれでもfragmentの代わりになる</div>
+            </>
+        );
+    }
+}
+
+class ClassComponents extends Component {
+    render() {
+        return (
+            <>
+                <div>クラスコンポーネントはあまり使われていないらしい。</div>
+                <div>propsを使用するときはthisが必要</div>
+            </>
+        );
+    }
+}
+
+const FunctionComponents = () => {
     return (
-        <div>
-            <Header methodsForState={() => methodsForState(null)} />
-            <Body methodsForState={(set) => methodsForState(set)} set={forState} />
-        </div>
+        <>
+            <div>今の主流は関数コンポーネント</div>
+            <div>propsを使用するときthisが必要ない</div>
+        </>
     );
 };
 
-// ========================================
-
-ReactDOM.render(<Room />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.querySelector('#root'));
