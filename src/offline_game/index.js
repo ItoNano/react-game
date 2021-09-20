6bf15408fce8123bce6d055e90399df0a3834895
itoNano <itokey120+1@gmail.com>
2021-09-20 18:32:49 +0900

    オフラインの時firebaseログイン処理を一度だけにする

--- src/offline_game/index.js
+++ src/offline_game/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
@@ -20,26 +20,28 @@ const OfflineGame = (props) => {
         O: [1, 1, 2, 2, 3, 3],
         set: null,
     });
-    if (process.env.REACT_APP_MODE !== 'local') {
-        const firebaseConfig = {
-            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-            databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.REACT_APP_FIREBASE_STOROGA_BUCKET,
-            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.REACT_APP_FIREBASE_APP_ID,
-            measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-        };
-        if (firebase.apps.length === 0) {
-            firebase.initializeApp(firebaseConfig);
+    useEffect(() => {
+        if (process.env.REACT_APP_MODE !== 'local') {
+            const firebaseConfig = {
+                apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+                authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+                databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+                projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+                storageBucket: process.env.REACT_APP_FIREBASE_STOROGA_BUCKET,
+                messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.REACT_APP_FIREBASE_APP_ID,
+                measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+            };
+            if (firebase.apps.length === 0) {
+                firebase.initializeApp(firebaseConfig);
+            }
+            const ref = firebase.database().ref('offline');
+            const today = new Date();
+            ref.push({
+                timestamp: today.toLocaleString(),
+            });
         }
-        const ref = firebase.database().ref('offline');
-        const today = new Date();
-        ref.push({
-            timestamp: today.toLocaleString(),
-        });
-    }
+    }, []);
     const handleClick = (i) => {
         const history = gameState.history.slice(0, gameState.stepNumber + 1);
         const current = history[gameState.stepNumber];
